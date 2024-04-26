8dcd60775c4cca9ae9ca9ed0896a4d25e16c4427
Haziq Roslee <haziq@Haziqs-MacBook-Pro.local>
2024-04-26 21:09:09 +0800

    feat: add refresh button & menu to only open after typing

--- src/pages/Bus.jsx
+++ src/pages/Bus.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import BusArrival from "../components/BusArrival";
+import ReactSelect from "react-select";
 
 export default function Bus() {
   const [busStops, setBusStops] = useState([]);
   const [selected, setSelected] = useState("");
   const [details, setDetails] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
 
   useEffect(() => {
     const fetchBusStop = async () => {
@@ -21,22 +23,23 @@ export default function Bus() {
     fetchBusStop();
   }, []);
 
+  const fetchDetails = async () => {
+    if (selected) {
+      const selectedNo = selected.value.split("-")[1].trim();
+      const response = await fetch(
+        `https://arrivelah2.busrouter.sg/?id=${selectedNo}`
+      );
+      const data = await response.json();
+      setDetails(data.services);
+    }
+  };
+
   useEffect(() => {
-    const fetchDetails = async () => {
-      if (selected) {
-        const selectedNo = selected.split("-")[1].trim();
-        const response = await fetch(
-          `https://arrivelah2.busrouter.sg/?id=${selectedNo}`
-        );
-        const data = await response.json();
-        setDetails(data.services);
-      }
-    };
     fetchDetails();
   }, [selected]);
 
-  const handleChange = (event) => {
-    setSelected(event.target.value);
+  const handleChange = (selected) => {
+    setSelected(selected);
   };
 
   const sortByArrivalTime = () => {
@@ -53,17 +56,32 @@ export default function Bus() {
     setDetails(sortedDetails);
   };
 
+  const handleRefresh = () => {
+    fetchDetails();
+  };
+
+  const handleInputChange = (inputValue) => {
+    setSearchValue(inputValue);
+  };
+
   return (
     <>
       <h1>Bus Arrival</h1>
-      <select onChange={handleChange}>
-        <option value="">Select a Bus Stop!</option>
-        {busStops.map((stop) => (
-          <option>{`${stop.name} - ${stop.stopNumber}`}</option>
-        ))}
-      </select>
+      <ReactSelect
+        value={selected}
+        menuIsOpen={!!searchValue} //* ChatGPT to open menu only after typing
+        isSearchable
+        placeholder="Search for bus stop.."
+        onChange={handleChange}
+        onInputChange={handleInputChange}
+        options={busStops.map((stop) => ({
+          value: `${stop.name} - ${stop.stopNumber}`,
+          label: `${stop.name} - ${stop.stopNumber}`,
+        }))}
+      />
       <button onClick={sortByArrivalTime}>Sort Arrival</button>
       <button onClick={sortByBusNumber}>Sort Bus Number</button>
+      <button onClick={handleRefresh}>Refresh List</button>
       <BusArrival details={details} />
     </>
   );
