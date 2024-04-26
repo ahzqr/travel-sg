c7550b8d85b04079ecc38b359d055cdcf8f9a70b
Haziq Roslee <haziq@Haziqs-MacBook-Pro.local>
2024-04-26 21:10:36 +0800

    feat: add key to BusArrival div

--- src/components/BusArrival.jsx
+++ src/components/BusArrival.jsx
@@ -1,8 +1,8 @@
-export default function BusArrival({details}) {
+export default function BusArrival({ details }) {
   return (
     <>
       {details.map((detail) => (
-        <div>
+        <div key={detail.no}>
           <p>
             Bus Number: <strong>{detail.no}</strong>, Arriving:{" "}
             <strong>{Math.floor(detail.next.duration_ms / 60000)} min</strong>
